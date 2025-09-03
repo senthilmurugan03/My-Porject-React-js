8de145ec531e7bf34cde3e209d0f9d80c766f565
senthilmurugan03 <senthilsenthil1920@gmail.com>
2025-09-04 02:00:28 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,34 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css'
-import Navigate01 from './Component/Navigate';
-import Usestate_01 from './Component/Usestate';
+import Usestate_01 from './Component/Usestate_01';
 import TernaryOp from './Component/TernaryOp';
 import Qustion from './Component/Qustion';
 import Form from './Component/Form';
-import UseEffect_Page from './Component/Useeffect';
-import UseEffect_reload from './Component/UseEffect_reload';
-import UseEffect_Fetch from './Component/UseEffectFetch';
 import UseEffect_Fetch_Reload from './Component/UseEffect_Fetch_Reload';
-
+import UseEffect_Fetch from './Component/UseEffect_Fetch';
+import Navigate01 from './Component/Nabvigat';
+import UseEffect_Page from './Component/UseEffect_Page';
+import UseEffect_reload from './Component/UseEffect_reload';
 
 function App() {
-
   return (
     <>
-      <div >
-
+      <div>
         <header><Navigate01 /></header>
 
         <Routes>
-          <Route path='Usestate_01' element={<Usestate_01 />}>Usestate</Route>
-          <Route path='TernaryOp' element={<TernaryOp />}>TernaryOp</Route>
-          <Route path='Qustion' element={<Qustion />}>Qustion</Route>
-          <Route path='Form' element={<Form />}>Form</Route>
-          <Route path='UseEffect_Page' element={<UseEffect_Page />}>Form</Route>
-          <Route path='UseEffect_reload' element={<UseEffect_reload />}>Form</Route>
-           <Route path='UseEffect_Fetch' element={<UseEffect_Fetch />}>Form</Route>
-            <Route path='UseEffect_Fetch_Reload' element={<UseEffect_Fetch_Reload />}>Form</Route>
+          <Route path='/' element={<Usestate_01 />} />
+          <Route path='TernaryOp' element={<TernaryOp />} />
+          <Route path='Qustion' element={<Qustion />} />
+          <Route path='Form' element={<Form />} />
+          <Route path='UseEffect_Page' element={<UseEffect_Page />} />
+          <Route path='UseEffect_reload' element={<UseEffect_reload />} />
+          <Route path='UseEffect_Fetch' element={<UseEffect_Fetch />} />
+          <Route path='UseEffect_Fetch_Reload' element={<UseEffect_Fetch_Reload />} />
         </Routes>
-
       </div>
-
     </>
   )
 }
 
-export default App;                                                                                                                                                                               
+export default App;
